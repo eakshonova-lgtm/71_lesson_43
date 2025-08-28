9f1796a21f742e061eb6082a124118160d3f2418
Ekaterina Akshonova <eakshonova@gmail.com>
2025-08-28 21:01:57 +0200

    2

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,6 +1,6 @@
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         System.out.println("This is the new version");
-        System.out.println("Updating class Test1");
+
     }
 }
